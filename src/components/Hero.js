e86b337263cf394cba0c1a2edebae448581707ef
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-08-13 16:38:04 +0200

    really fixed Hero issue now

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,20 +11,22 @@ import OptImage from './OptImage';
 import Button from './Button';
 
 const Hero = () => {
-  const { html, cta1, cta2, cta1Link, cta2Link, image } = useHeroContent();
+  const { html, cta1, cta2, image } = useHeroContent();
 
   return (
-    <StyledHero className="container">
-      <div className="row">
-        <div className="text-wrapper col-md-6">
-          <div dangerouslySetInnerHTML={{ __html: html }} />
-          <div className="buttons-wrapper">
-            <Button color="dark" text={cta1} />
-            <Button text={cta2} />
+    <StyledHero>
+      <div className="container">
+        <div className="row">
+          <div className="text-wrapper col-md-6">
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <div className="buttons-wrapper">
+              <Button color="dark" text={cta1} />
+              <Button text={cta2} />
+            </div>
           </div>
-        </div>
-        <div className="col-md-6 hero-image-wrapper">
-          <OptImage alt="hero-image" filename={image} />
+          <div className="col-md-6 hero-image-wrapper">
+            <OptImage alt="hero-image" filename={image} />
+          </div>
         </div>
       </div>
     </StyledHero>
